e78f5879f94cba8c67e0ebcb4f1b0a51b1c4ad72
Chris Smith <chris@chameth.com>
2016-12-05 19:44:46 +0000

    Day 5

---
+++ 05.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import hashlib
+import itertools
+from multiprocessing import Pool
+
+door = 'abbhdwsy'
+
+def hash(x):
+    return hashlib.md5((door + str(x)).encode('utf-8')).hexdigest()
+
+pool = Pool()
+hashes = pool.imap(hash, itertools.count(), 10000)
+zeroes = (h for h in hashes if h.startswith('00000'))
+part1 = ''
+part2 = ['_'] * 8
+
+for h in zeroes:
+    if len(part1) < 8:
+        part1 += h[5]
+    if h[5].isdigit() and int(h[5]) < len(part2) and part2[int(h[5])] == '_':
+        part2[int(h[5])] = h[6]
+
+    if '_' not in part2:
+        print("Part one: %s" % part1)
+        print("Part two: %s" % ''.join(part2))
+        break
