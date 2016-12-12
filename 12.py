0fb2f5eecc6efb9c7f96f4984ffc7d52b3028682
Chris Smith <chris@chameth.com>
2016-12-12 08:32:56 +0000

    Day 12

---
+++ 12.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+with open('12.txt', 'r') as file:
+    instr = list(map(str.split, map(str.strip, file.readlines())))
+
+def value(x):
+    try:
+        return int(x)
+    except ValueError:
+        return registers[x]
+
+def cpy(args): registers[args[1]] = value(args[0])
+def inc(args): registers[args[0]] += 1
+def dec(args): registers[args[0]] -= 1
+def jnz(args): global pc; pc += (0 if value(args[0]) == 0 else value(args[1]) - 1)
+
+def run():
+    global pc
+    while pc < len(instr):
+        globals()[instr[pc][0]](instr[pc][1:])
+        pc += 1
+
+registers = {'a': 0, 'b': 0, 'c': 0, 'd': 0}
+pc = 0
+run()
+
+print("Stage 1: %s" % registers['a'])
+
+registers = {'a': 0, 'b': 0, 'c': 1, 'd': 0}
+pc = 0
+run()
+
+print("Stage 2: %s" % registers['a'])
