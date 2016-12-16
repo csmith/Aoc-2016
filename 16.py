8ff910b26e36a2e84cce7db994f6b8db5289b4a4
Chris Smith <chris@chameth.com>
2016-12-16 09:43:29 +0000

    Day 16.

---
+++ 16.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import itertools
+
+
+def expand(state, length):
+    while len(state) < length:
+        state = list(itertools.chain(state, [False], (not x for x in reversed(state))))
+    return state[:length]
+
+
+def checksum_round(state):
+    return [state[i] == state[i + 1] for i in range(0, len(state), 2)]
+
+
+def checksum(state):
+    state = checksum_round(state)
+    while len(state) % 2 == 0:
+        state = checksum_round(state)
+    return state
+
+
+def run(state, size):
+    return ''.join(str(int(x)) for x in checksum(expand(state, size)))
+
+
+iv = [bool(int(x)) for x in '01110110101001000']
+print('Step 1: %s' % run(iv, 272))
+print('Step 2: %s' % run(iv, 35651584))
