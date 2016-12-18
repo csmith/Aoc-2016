39f4419ee0c47396616f609ac0bbb67bacffc3af
Chris Smith <chris87@gmail.com>
2016-12-18 18:51:43 +0000

    Code golf day 18 a little.

--- 18.py
+++ 18.py
@@ -5,30 +5,22 @@
 Each line of the maze is represented as a list of booleans, with True for safe spaces and False for traps.
 
 The four separate trap rules can be simplified to just "left is different to right", which is handled in the line
-method (with some annoying fudging to handle the first and last elements whose parents are 'walls').
+lambda (with some annoying fudging to handle the first and last elements whose parents are 'walls').
 
 The solution is obtained by repeatedly generating lines and summing them (Python booleans are a subclass of ints,
 with True being equal to 1 and False to 0, so a simple call to sum counts the number of safe squares). Only the most
-recent line is kept, everything else is discarded once counted.
+recent line is kept, everything else is discarded once counted. This is implemented using a call to functools.reduce,
+passing along a tuple of (line, sum). In each round, the line is mutated to its new form, and the sum of safe spaces
+is added to the running total.
 """
+
+import functools
 
 seed = [x == '.' for x in
         '^^.^..^.....^..^..^^...^^.^....^^^.^.^^....^.^^^...^^^^.^^^^.^..^^^^.^^.^.^.^.^.^^...^^..^^^..^.^^^^']
 
+line = lambda p: [(i == 0 or p[i - 1]) == (i == len(p) - 1 or p[i + 1]) for i in range(len(p))]
+calc = lambda n: functools.reduce(lambda x, y: (line(x[0]), x[1] + sum(line(x[0]))), range(n - 1), (seed, sum(seed)))[1]
 
-def line(previous):
-    return [(i == 0 or previous[i - 1]) == (i == len(previous) - 1 or previous[i + 1]) for i in range(len(previous))]
-
-
-def calculate(num):
-    last_line = seed
-    safe_spaces = sum(last_line)
-    lines = 1
-    while lines < num:
-        last_line = line(last_line)
-        safe_spaces += sum(last_line)
-        lines += 1
-    return safe_spaces
-
-print('Part one: %s' % calculate(40))
-print('Part two: %s' % calculate(400000))
+print('Part one: %s' % calc(40))
+print('Part two: %s' % calc(400000))
